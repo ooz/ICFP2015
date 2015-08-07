9d04b0810ffb6d32df313e00a87ff4ed52b74421
ooz <oliverzscheyge@gmail.com>
2015-08-08 01:45:49 +0200

    Refactor RNG

--- src/lcg.py
+++ src/lcg.py
@@ -11,23 +11,26 @@ class LCG(object):
         self.mul = 1103515245
         self.inc = 12345
 
-    def val(self):
-        ret = '{0:031b}'.format(self._val)
+    def __iter__(self):
+        return self
+
+    def val(self, val):
+        ret = '{0:031b}'.format(val)
         ret = ret[::-1]
         ret = ret[16:31]
         ret = ret[::-1]
         return int(ret, 2)
 
     def next(self):
+        ret = self._val
         self._val = (self.mul * self._val + self.inc) % self.mod
-        return self.val()
+        return self.val(ret)
 
 
 def main():
     gen = LCG(17)
     for i in range(10):
-        print gen.val()
-        gen.next()
+        print gen.next()
 
 if __name__ == '__main__':
     main()
